80b901aab7224c997136c8d60336733bfddb48ea
anhtudo97 <anhtudo97@gmail.com>
2026-01-29 16:29:08 +0700

    feat: update layout

---
+++ apps/web/app/(dashboard)/integrations/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      Integrations Page
+    </div>
+  )
+}
+
+export default Page
--- apps/web/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ apps/web/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -1,11 +1,10 @@
 "use client"
 
-import React from 'react'
-import { InboxIcon, LibraryBigIcon, } from "lucide-react"
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarRail } from '@workspace/ui/components/sidebar'
-import { OrganizationSwitcher } from '@clerk/nextjs'
-import { usePathname } from 'next/navigation'
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarRail } from '@workspace/ui/components/sidebar'
+import { CreditCardIcon, InboxIcon, LayoutDashboardIcon, LibraryBigIcon, Mic, PaletteIcon } from "lucide-react"
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const customerSupportItems = [
   {
@@ -17,6 +16,32 @@ const customerSupportItems = [
     title: "Knowledge Base",
     url: "/files",
     icon: LibraryBigIcon
+  }
+]
+
+const configurationItems = [
+  {
+    title: "Widget Customization",
+    url: "/customization",
+    icon: PaletteIcon
+  },
+  {
+    title: "Integrations",
+    url: "/integrations",
+    icon: LayoutDashboardIcon
+  },
+  {
+    title: "Voice Assistant",
+    url: "/plugins/vapi",
+    icon: Mic
+  }
+]
+
+const accountItems = [
+  {
+    title: "Plans & Billing",
+    url: "/billing",
+    icon: CreditCardIcon
   }
 ]
 
@@ -35,12 +60,29 @@ const DashboardSidebar = () => {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton asChild size={'lg'}>
-              <OrganizationSwitcher hidePersonal skipInvitationScreen />
+              <OrganizationSwitcher
+                hidePersonal
+                skipInvitationScreen
+                appearance={
+                  {
+                    elements: {
+                      rootBox: "w-full! h-8!",
+                      avatarBox: "size-4! rounded-sm!",
+                      organizationSwitcherTrigger: "w-full! justify-start! group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2!",
+                      organizationPreview: "group-data-[collapsible=icon]:justify-center! gap-2!",
+                      organizationPreviewTextContainer: "group-data-[collapsible=icon]:hidden! text-xs! font-medium! text-sidebar-foreground!",
+                      organizationSwitcherTriggerIcon: "group-data-[collapsible=icon]:hidden! ml-auto! text-sidebar-foreground!"
+                    }
+                  }
+                }
+              />
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
+
+        {/* Customer support */}
         <SidebarGroup>
           <SidebarGroupLabel>Customer Support</SidebarGroupLabel>
           <SidebarGroupContent>
@@ -64,8 +106,80 @@ const DashboardSidebar = () => {
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+
+        {/* Configuration */}
+        <SidebarGroup>
+          <SidebarGroupLabel>Configuration</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {
+                configurationItems.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      asChild
+                      tooltip={item.title}
+                      isActive={isActive(item.url)}
+                    >
+                      <Link href={item.url}>
+                        <item.icon className='size-4' />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))
+              }
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* Account */}
+        <SidebarGroup>
+          <SidebarGroupLabel>Account</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {
+                accountItems.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      asChild
+                      tooltip={item.title}
+                      isActive={isActive(item.url)}
+                    >
+                      <Link href={item.url}>
+                        <item.icon className='size-4' />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))
+              }
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
-      <SidebarRail /> 
+
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <UserButton
+              showName
+              appearance={
+                {
+                  elements: {
+                    rootBox: "w-full! h-8!",
+                    userButtonTrigger: "w-full! p-2! hover:bg-sidebar-accent! hover:text-sidebar-foreground! group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2!",
+                    userButtonBox: "w-full! flex-row-reverse! justify-end! gap-2! group-data-[collapsible=icon]:justify-center! text-sidebar-foreground!",
+                    userButtonOuterIdentifier: "pl-0! group-data-[collapsible=icon]:hidden!",
+                    avatarBox: "size-5!", 
+                  }
+                }
+              }
+            />
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+
+      <SidebarRail />
     </Sidebar>
   )
 }
